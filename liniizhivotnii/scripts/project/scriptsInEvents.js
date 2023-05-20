f6a1ed823586008c9c1f667b753742b5cdb59867
samgamess <132680405+samgamess@users.noreply.github.com>
2023-05-20 23:03:26 +0300

    Add files via upload

---
+++ liniizhivotnii/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Menuevent_Event1_Act3(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#9b7142'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
