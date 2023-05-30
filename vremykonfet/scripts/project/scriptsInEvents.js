8f99f91b2aa19ef0401afd99696b4155e0478bf6
samgamess <132680405+samgamess@users.noreply.github.com>
2023-05-30 13:29:37 +0300

    Add files via upload

---
+++ vremykonfet/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEvents_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#55b410'
+	},
+
+	async MenuEvents_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#55b410'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
