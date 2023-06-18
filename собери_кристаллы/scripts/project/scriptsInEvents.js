308a4bf64917bb3422e30dd7eebb258fc17a7cf7
samgamess <132680405+samgamess@users.noreply.github.com>
2023-06-18 17:46:49 +0300

    Add files via upload

---
+++ собери_кристаллы/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEvents_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#7bc0d7'
+	},
+
+	async MenuEvents_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#94f8cc'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
